50987b44ad23dc4bd491d4334edb53f00bc70c30
kir.khi <garry4ever@gmail.com>
2018-09-19 12:40:36 +0300

    first commit

---
+++ src/main/java/com/tel_ran/cvBank/CvBankApplication.java
@@ -0,0 +1,12 @@
+package com.tel_ran.cvBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBankApplication.class, args);
+	}
+}
--- src/test/java/com/tel_ran/cvBank/CvBankApplicationTests.java
+++ src/test/java/com/tel_ran/cvBank/CvBankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.soft_dev.cvbank.cvbank;
+package com.tel_ran.cvBank;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CvbankApplicationTests {
+public class CvBankApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
